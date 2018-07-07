d8794ab206ca8acd815bfa89281ed5615a4a50db
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:31:54 +0100

    Pass the third test

--- III.tdd-katas/calculator-tdd/calculator.js
+++ III.tdd-katas/calculator-tdd/calculator.js
@@ -1,3 +1,11 @@
-module.exports = function add (string) {
+function safeParseToInt(string) {
     return string ? parseInt(string) : 0;
+}
+
+module.exports = function add (string) {
+    let total = safeParseToInt(string);
+    if (string.indexOf(',') > -1) {
+        total += safeParseToInt(string.split(',')[1]);
+    }
+    return total;
 }
