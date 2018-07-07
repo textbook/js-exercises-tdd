b4ac1d03a57147f09245cb8ab5a5f809f526561c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:45:27 +0100

    Extract common sum functionality

--- III.tdd-katas/calculator-tdd/calculator.js
+++ III.tdd-katas/calculator-tdd/calculator.js
@@ -2,6 +2,12 @@ function safeParseToInt(string) {
     return string ? parseInt(string) : 0;
 }
 
+function sum(values) {
+    return values.reduce(function (accumulator, value) {
+        return accumulator + value;
+    }, 0);
+}
+
 module.exports = function add (string) {
-    return string.split(',').map(safeParseToInt).reduce((a, b) => a + b, 0);
+    return sum(string.split(',').map(safeParseToInt));
 }
