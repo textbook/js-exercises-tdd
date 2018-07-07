b2a63f919f4e7637a5877c0f2022cf75e5922a68
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 14:03:15 +0100

    Refactor to make it clearer what's happening

--- III.tdd-katas/calculator-tdd/calculator.js
+++ III.tdd-katas/calculator-tdd/calculator.js
@@ -1,20 +1,25 @@
-function safeParseToInt(string) {
+function safeParseToInt (string) {
     return string ? parseInt(string) : 0;
 }
 
-function sum(values) {
+function sum (values) {
     return values.reduce(function (accumulator, value) {
         return accumulator + value;
     }, 0);
 }
 
+const delimiterMarker = '//';
+
+function getDelimiter (string) {
+    const firstLine = string.split(/\n/)[0];
+    return firstLine.slice(delimiterMarker.length,);
+}
+
 module.exports = function add (string) {
     let delimiter = ',';
-    if (string.indexOf('//') === 0) {
-        delimiter = string.split(/\n/)[0].slice(2,);
-        // remove slashes, delimiter and newline
-        string = string.slice(2 + delimiter.length + 1);
+    if (string.indexOf(delimiterMarker) === 0) {
+        delimiter = getDelimiter(string);
+        string = string.slice(delimiterMarker.length + delimiter.length);
     }
-    const splitter = new RegExp(`[${delimiter}\n]`);
-    return sum(string.split(splitter).map(safeParseToInt));
+    return sum(string.split(new RegExp(`[${delimiter}\n]`)).map(safeParseToInt));
 }
