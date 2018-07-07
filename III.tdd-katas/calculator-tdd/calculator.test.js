994dfeaf9a9711e1757faea8eaf0f5a38600d6c6
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:29:41 +0100

    Third failing test - two number case

--- III.tdd-katas/calculator-tdd/calculator.test.js
+++ III.tdd-katas/calculator-tdd/calculator.test.js
@@ -7,3 +7,7 @@ test('returns 0 for empty string', function () {
 test('returns the number for a single number', function () {
     expect(add('1')).toBe(1);
 });
+
+test('returns the sum of numbers for two numbers', function () {
+    expect(add('1,2')).toBe(3);
+});
