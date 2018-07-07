19edf29acec27d3abe7639d6bea27278f5a5940c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:08:47 +0100

    Second failing test - single number case

--- III.tdd-katas/calculator-tdd/calculator.test.js
+++ III.tdd-katas/calculator-tdd/calculator.test.js
@@ -3,3 +3,7 @@ const add = require('./calculator');
 test('returns 0 for empty string', function () {
     expect(add('')).toBe(0);
 });
+
+test('returns the number for a single number', function () {
+    expect(add('1')).toBe(1);
+});
