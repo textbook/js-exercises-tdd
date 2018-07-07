5dcb08d2698b5d4a82eefaab2c867d998cc38263
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:33:22 +0100

    Fourth failing test - many number case

--- III.tdd-katas/calculator-tdd/calculator.test.js
+++ III.tdd-katas/calculator-tdd/calculator.test.js
@@ -1,13 +1,21 @@
 const add = require('./calculator');
 
-test('returns 0 for empty string', function () {
-    expect(add('')).toBe(0);
+describe('Step 1', function () {
+    test('returns 0 for empty string', function () {
+        expect(add('')).toBe(0);
+    });
+    
+    test('returns the number for a single number', function () {
+        expect(add('1')).toBe(1);
+    });
+    
+    test('returns the sum of numbers for two numbers', function () {
+        expect(add('1,2')).toBe(3);
+    });
 });
 
-test('returns the number for a single number', function () {
-    expect(add('1')).toBe(1);
-});
-
-test('returns the sum of numbers for two numbers', function () {
-    expect(add('1,2')).toBe(3);
+describe('Step 2', function () {
+    test('handles an unknown amount of numbers', function () {
+        expect(add('1,2,3,4')).toBe(10);
+    });
 });
