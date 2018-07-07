2c5375f25bc467af3f1cb5af376a664c8f8e1e51
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-07 13:55:19 +0100

    Pass the sixth test

--- III.tdd-katas/calculator-tdd/calculator.js
+++ III.tdd-katas/calculator-tdd/calculator.js
@@ -9,5 +9,12 @@ function sum(values) {
 }
 
 module.exports = function add (string) {
-    return sum(string.split(/[,\n]/).map(safeParseToInt));
+    let delimiter = ',';
+    if (string.indexOf('//') === 0) {
+        delimiter = string.split(/\n/)[0].slice(2,);
+        // remove slashes, delimiter and newline
+        string = string.slice(2 + delimiter.length + 1);
+    }
+    const splitter = new RegExp(`[${delimiter}\n]`);
+    return sum(string.split(splitter).map(safeParseToInt));
 }
