d769fd555b3e3c198a5b6eee247c82ae8201b4d3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-08 10:19:07 +0100

    Add method for finding numerical characters

--- III.tdd-katas/password-verifier/password.js
+++ III.tdd-katas/password-verifier/password.js
@@ -10,9 +10,14 @@ function lowercase (string) {
     return matches(string, letter => letter.toLowerCase());
 }
 
+function numerical (string) {
+    return string.split('').filter(letter => !isNaN(parseInt(letter)));
+}
+
 module.exports = function verify (password) {
     return !!password 
         && password.length > 8 
         && uppercase(password).length > 0
-        && lowercase(password).length > 0;
+        && lowercase(password).length > 0
+        && numerical(password).length > 0;
 }
