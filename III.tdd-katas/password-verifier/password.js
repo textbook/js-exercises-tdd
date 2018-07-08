a9f47bb05941e3ce19697e597e27ed7a5d4bc21c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-08 10:12:11 +0100

    Duplicate uppercase functionality for lowercase

--- III.tdd-katas/password-verifier/password.js
+++ III.tdd-katas/password-verifier/password.js
@@ -2,8 +2,13 @@ function uppercase (string) {
     return string.split('').filter(letter => letter == letter.toUpperCase());
 }
 
+function lowercase (string) {
+    return string.split('').filter(letter => letter == letter.toLowerCase());
+}
+
 module.exports = function verify (password) {
     return !!password 
         && password.length > 8 
-        && uppercase(password).length > 0;
+        && uppercase(password).length > 0
+        && lowercase(password).length > 0;
 }
