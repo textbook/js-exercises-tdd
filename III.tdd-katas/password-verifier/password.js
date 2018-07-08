de28cc574e743a6e36313fe85366343784c48c2b
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-08 10:09:30 +0100

    Extract generic uppercase functionality

--- III.tdd-katas/password-verifier/password.js
+++ III.tdd-katas/password-verifier/password.js
@@ -1,5 +1,9 @@
+function uppercase (string) {
+    return string.split('').filter(letter => letter == letter.toUpperCase());
+}
+
 module.exports = function verify (password) {
     return !!password 
         && password.length > 8 
-        && password.split('').filter(letter => letter == letter.toUpperCase()).length > 0;
+        && uppercase(password).length > 0;
 }
