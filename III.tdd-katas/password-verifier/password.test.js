f68064a77795cecb9fc82668f87d72806b0cdd17
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-08 09:59:12 +0100

    Failing test for minimum length requirement

--- III.tdd-katas/password-verifier/password.test.js
+++ III.tdd-katas/password-verifier/password.test.js
@@ -4,4 +4,10 @@ describe('Password Verifier', () => {
     it('returns false for null', () => {
         expect(verify(null)).toBe(false);
     });
+
+    describe('length requirements', () => {
+        it('returns true for length greater than 8', () => {
+            expect(verify('ninechars')).toBe(true);
+        });
+    });
 });
