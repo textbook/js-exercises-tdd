6803fb01be36bc4020b5021292ea082fa427047a
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-08 10:08:44 +0100

    Add logic to count uppercase letters

--- III.tdd-katas/password-verifier/password.js
+++ III.tdd-katas/password-verifier/password.js
@@ -1,3 +1,5 @@
 module.exports = function verify (password) {
-    return !!password && password.length > 8;
+    return !!password 
+        && password.length > 8 
+        && password.split('').filter(letter => letter == letter.toUpperCase()).length > 0;
 }
